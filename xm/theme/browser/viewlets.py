f53ae64b51be62f26f2a6f1896256ab3f9d8204a
markvl <markvl@localhost>
2008-01-14 16:37:03 +0000

    Added first version of project header viewlet.
    svn path=/xm.theme/trunk/; revision=56890

--- xm/theme/browser/viewlets.py
+++ xm/theme/browser/viewlets.py
@@ -1,3 +1,12 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
+class ProjectHeader(ViewletBase):
+    render = ViewPageTemplateFile('project_header.pt')
+
+    def project_title(self):
+        try:
+            title = self.context.getProject().title
+        except AttributeError:
+            title = 'eXtremeManagement'
+        return title
