f0a0b5194ec9e0b838d147bfac8b8cdddc3fcfd8
markvl <markvl@localhost>
2008-02-21 16:37:52 +0000

    Draft of view to show specific content types in a project.
    svn path=/xm.theme/trunk/; revision=59149

--- xm/theme/browser/interfaces.py
+++ xm/theme/browser/interfaces.py
@@ -1,11 +1,43 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope.viewlet.interfaces import IViewletManager
+from zope.publisher.interfaces.browser import IBrowserView
+from zope.interface import Interface
+from zope.interface import directlyProvides
+from zope.contentprovider.interfaces import ITALNamespaceData
+from zope.schema import Text
+
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
 
+
 class IProjectTabsManager(IViewletManager):
     """A viewlet manager which will be used to display the tabs for
     a project.
     """
+
+
+class IProjectContentView(IBrowserView):
+    """Return information about the contents of a project."""
+
+    def requested_type():
+        """Return the requested portal type."""
+
+    def requested_state():
+        """Return the requested state."""
+
+    def title():
+        """Return the title for the page."""
+
+
+class IProjectContentProvider(Interface):
+    """Content provider to show the contents of a project."""
+
+    portal_type = Text(title = u'Portal type to filter on.')
+    state = Text(title = u'State to filter on.')
+
+    def iterations():
+        """Return the iterations."""
+
+directlyProvides(IProjectContentProvider, ITALNamespaceData)
---
+++ xm/theme/browser/project.py
@@ -0,0 +1,121 @@
+from Acquisition import Explicit
+from Acquisition import aq_inner
+
+from zope.component import adapts
+from zope.interface import implements
+from zope.interface import Interface
+from zope.contentprovider.interfaces import IContentProvider
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.publisher.interfaces.browser import IBrowserView
+from plone.memoize.view import memoize
+
+from xm.theme.browser.interfaces import IProjectContentView
+from xm.theme.browser.interfaces import IProjectContentProvider
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ProjectContentView(BrowserView):
+    """Return information about the contents of a project."""
+    implements(IProjectContentView)
+
+    @memoize
+    def requested_type(self):
+        """Return the requested portal type."""
+        return self.request.form.get('type', None)
+
+    @memoize
+    def requested_state(self):
+        """Return the requested state."""
+        return self.request.form.get('state', None)
+
+    def title(self):
+        """Return the title for the page."""
+        context = aq_inner(self.context)
+        portal_type = self.requested_type()
+        type_map = {'iteration': u'Iterations',
+                    'offer': u'Offers',
+                    'tracker': u'Issue trackers',
+                    'other': u'Attachments',
+                    }
+        title = context.Title()
+        if portal_type:
+            type_title = type_map.get(portal_type)
+            if type_title:
+                title += ': %s' % type_title
+        return title
+
+
+class ProjectContentProvider(Explicit):
+    """View to show specific items contained in a project."""
+    implements(IContentProvider, IProjectContentProvider)
+    adapts(Interface, IDefaultBrowserLayer, IBrowserView)
+
+    portal_type = None
+    state = None
+
+    def __init__(self, context, request, view):
+        self.context = context
+        self.request = request
+        self.__parent__ = view
+        self.project_view = aq_inner(context).restrictedTraverse('@@project')
+
+    def update(self):
+        pass
+
+    render = ViewPageTemplateFile('templates/project_content.pt')
+
+    def _get_iteration(self):
+        """Return the iterations.
+        Optionally take the state into account, when requested.
+        """
+        state_map = {'open': ('new', ),
+                     'closed': ('completed', 'invoiced'),
+                     'current': ('in-progress', ),
+                     }
+        states = state_map.get(self.state, None)
+        return self.project_view.getIterations(states)
+
+    def _get_offer(self):
+        """Return the offers."""
+        return self.project_view.offers()
+
+    def _get_tracker(self):
+        """Return the issue trackers."""
+        context = aq_inner(self.context)
+        cfilter = dict(portal_type='PoiTracker')
+        brains = context.getFolderContents(cfilter)
+        tracker_list = []
+        for brain in brains:
+            tracker_list.append(dict(title = brain.Title,
+                                     url = brain.getURL(),
+                                     ))
+        return tracker_list
+
+    def _get_other(self):
+        """Return all other content of a project,
+        not being iterations, offers or trackers.
+        """
+        context = aq_inner(self.context)
+        content_types = set([t.content_meta_type for t in
+                         context.allowedContentTypes()])
+        ignore_types = set(['Iteration', 'Offer', 'PoiTracker'])
+        filter_types = tuple(content_types - ignore_types)
+        cfilter = dict(meta_type = filter_types)
+        brains = context.getFolderContents(cfilter)
+        result_list = []
+        for brain in brains:
+            result_list.append(dict(title = brain.Title,
+                                    url = brain.getURL(),
+                                    ))
+        return result_list
+
+    @memoize
+    def items(self):
+        """Return the items to be shown."""
+        method = '_get_%s' % str(self.portal_type).lower()
+        if method in dir(self):
+            return self.__getattribute__(method)()
+        else:
+            return []
