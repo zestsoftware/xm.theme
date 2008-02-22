1bfa6bbf8db9df7f58994fc6c70f741bf3543aff
markvl <markvl@localhost>
2008-02-22 14:30:55 +0000

    Change the portlet configuration.
    svn path=/xm.theme/trunk/; revision=59188

--- xm/theme/setuphandlers.py
+++ xm/theme/setuphandlers.py
@@ -1,11 +1,47 @@
-def setupVarious(context):
+from zope.app.container.interfaces import INameChooser
+from zope.component import getUtility, getMultiAdapter
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.app.portlets import portlets as plone_portlets
 
-    # Ordinarily, GenericSetup handlers check for the existence of XML files.
-    # Here, we are not parsing an XML file, but we use this text file as a
-    # flag to check that we actually meant for this import step to be run.
-    # The file is found in profiles/default.
 
+def configure_portlets(site, logger):
+    """Set up the portlets."""
+    # Get some definitions.
+    column = getUtility(IPortletManager, name="plone.leftcolumn", context=site)
+    manager = getMultiAdapter((site, column), IPortletAssignmentMapping)
+    chooser = INameChooser(manager)
+
+    # First we'll remove everythin to get started with a clean slate.
+    for key in manager.keys():
+        del manager[key]
+        logger.info('Removed portlet "%s" from the left column' % key)
+    # Then we can add the portlets we actually need.
+    assignments = [
+        plone_portlets.login.Assignment(),
+        plone_portlets.classic.Assignment('portlet_poi', 'portlet'),
+        plone_portlets.classic.Assignment('portlet_tasks', 'portlet'),
+        plone_portlets.classic.Assignment('portlet_stories', 'portlet'),
+        ]
+    for assignment in assignments:
+        title = assignment.title
+        if title not in [item.title for item in manager.values()]:
+            manager[chooser.chooseName(title, assignment)] = assignment
+            logger.info('Added portlet "%s" to left column.', title)
+
+    # We do not need portlets on the right at all.
+    column = getUtility(IPortletManager, name="plone.rightcolumn",
+                        context=site)
+    manager = getMultiAdapter((site, column), IPortletAssignmentMapping)
+    for key in manager.keys():
+        del manager[key]
+        logger.info('Removed portlet "%s" from the right column' % key)
+
+
+def setup_various(context):
+    """Perform various setup steps."""
     if context.readDataFile('xm.theme_various.txt') is None:
         return
-
-    # Add additional setup code here
+    logger = context.getLogger('xm.theme')
+    site = context.getSite()
+    configure_portlets(site, logger)
