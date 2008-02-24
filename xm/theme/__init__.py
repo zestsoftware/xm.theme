ed1def166cbe0c1395d259c920640596e4202364
markvl <markvl@localhost>
2008-02-24 10:26:22 +0000

    Bugfix. Now the login_next script can actually import the xmMessageFactory.
    svn path=/xm.theme/trunk/; revision=59292

--- xm/theme/__init__.py
+++ xm/theme/__init__.py
@@ -2,5 +2,10 @@ from zope.i18nmessageid import MessageFactory
 
 xmMessageFactory = MessageFactory('xm.theme')
 
+# Allow restricted Python to import the  module
+from AccessControl import allow_module
+allow_module('xm.theme')
+
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
