659ce093e835b8d3dbd5aa514b0249570edc506c
Maurits van Rees <maurits@vanrees.org>
2008-09-03 18:33:29 +0000

    We have two locales now, so the rebuild i18n script should take that into account.
    svn path=/xm.theme/trunk/; revision=71173

--- xm/theme/rebuild_i18n.sh
+++ xm/theme/rebuild_i18n.sh
@@ -6,6 +6,11 @@ I18NDOMAIN=$PRODUCTNAME
 i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
-i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
+i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
-msgfmt -o locales/nl/LC_MESSAGES/${PRODUCTNAME}.mo locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
+# Only needed for Plone 3.0, not 3.1:
+for lang in locales/*; do
+    if test -d lang; then
+        msgfmt -o lang/LC_MESSAGES/${PRODUCTNAME}.mo lang/LC_MESSAGES/${PRODUCTNAME}.po
+    fi
+done
