0a3bbae57778a690ed644696537be4002b3575f6
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2008-11-22 07:31:48 +0000

    The username in the personal bar now links to the author page.
    svn path=/xm.theme/trunk/; revision=76310

--- xm/theme/browser/viewlets.py
+++ xm/theme/browser/viewlets.py
@@ -2,6 +2,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from zope.component import getMultiAdapter
+from urllib import quote_plus
 
 
 class XMProjectHeaderViewlet(ViewletBase):
@@ -60,3 +61,35 @@ class XMSearchBoxViewlet(ViewletBase):
 class GoogleSearchViewlet(ViewletBase):
 
         render = ViewPageTemplateFile('templates/googlesearch.pt')
+
+
+class XMPersonalBarViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/personal_bar.pt')
+
+    def update(self):
+        super(XMPersonalBarViewlet, self).update()
+
+        context_state = getMultiAdapter((self.context, self.request),
+                                         name=u'plone_context_state')
+        tools = getMultiAdapter((self.context, self.request),
+                                 name=u'plone_tools')
+        self.user_actions = context_state.actions().get('user', None)
+        plone_utils = getToolByName(self.context, 'plone_utils')
+        self.getIconFor = plone_utils.getIconFor
+        self.anonymous = self.portal_state.anonymous()
+        if not self.anonymous:
+            member = self.portal_state.member()
+            userid = member.getId()
+            self.homelink_url = self.site_url + '/author/' + quote_plus(userid)
+
+            member_info = tools.membership().getMemberInfo(member.getId())
+            # member_info is None if there's no Plone user object, as when
+            # using OpenID.
+            if member_info:
+                fullname = member_info.get('fullname', '')
+            else:
+                fullname = None
+            if fullname:
+                self.user_name = fullname
+            else:
+                self.user_name = userid
